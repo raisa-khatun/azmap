8ba1772576f5c992b08aa4ded481b953fd408696
raisa-khatun <raisakhatun@gmail.com>
2020-01-07 23:00:45 -0700

    Showing points from json file instead of hardcoding

--- Azmap_v1/App.js
+++ Azmap_v1/App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -28,16 +28,12 @@ import {
 import MapView from 'react-native-maps';
 
 
+
 const App: () => React$Node = () => {
 
-  var markers = [
-    {
-      latitude: 33.45179,
-      longitude: -112.022179,
-      title: 'Foo Place',
-      subtitle: '1234 Foo Drive'
-    }
-  ];
+  const [med_centers, setMed]=useState(require('./data/med_centers.json'));
+
+  console.log(med_centers);
 
   return (
     //<>
@@ -52,80 +48,22 @@ const App: () => React$Node = () => {
           zoomEnabled:true,
 
         }}
-        //annotations={markers}
+
     >
-    <MapView.Marker
-                coordinate={{latitude: 33.45179,
-                longitude: -112.022179}}
-                title={"2770 E Van Buren St.Phoenix, AZ 85008"}
-                description={"(602) 273-9999"}
-     />
-     <MapView.Marker
-                    coordinate={{latitude: 33.34047,
-                    longitude:-111.68531}}
-                    title={"1550 N Stonehedge Dr., Ste. 104 Gilber, AZ 85233"}
-                    description={"(480) 962-7922"}
-         />
-    <MapView.Marker
-                    coordinate={{latitude: 33.43589,
-                    longitude:-112.07486}}
-                    title={"8825 N 23rd Ave., Ste. 100 Phoenix, AZ 85021"}
-                    description={"(480) 962-7922"}
-    />
-    <MapView.Marker
-                    coordinate={{latitude: 33.5653,
-                    longitude:-112.10766}}
-                    title={"8825 N 23rd Ave., Ste. 100 Phoenix, AZ 85021"}
-                    description={"(480) 962-7922"}
-   />
-     </MapView>
+   {med_centers.map(point => {
+    console.log(point.latitude);
+                       return  <MapView.Marker
+                           key={point.id}
+                           coordinate={{latitude: parseFloat(point.latitude), longitude:parseFloat(point.longitude)}}
+                           title={point.title}
+                           description={point.description}
+                         />
+    })}
 
-    </View>
-    /*
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+   </MapView>
 
-    </>
-    */
+   </View>
+
   );
 };
 
@@ -140,50 +78,7 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     }
-  /*
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  map:{
-    flex:1
-  },
-  container:{
-    flex:1
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-  */
+
 });
 
 export default App;
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # azmap
+
+npx react-native run-android
+
+<MapView.Marker
+                coordinate={{latitude: 33.45179,
+                longitude: -112.022179}}
+                title={"2770 E Van Buren St.Phoenix, AZ 85008"}
+                description={"(602) 273-9999"}
+     />
+     <MapView.Marker
+                    coordinate={{latitude: 33.34047,
+                    longitude:-111.68531}}
+                    title={"1550 N Stonehedge Dr., Ste. 104 Gilber, AZ 85233"}
+                    description={"(480) 962-7922"}
+         />
+    <MapView.Marker
+                    coordinate={{latitude: 33.43589,
+                    longitude:-112.07486}}
+                    title={"8825 N 23rd Ave., Ste. 100 Phoenix, AZ 85021"}
+                    description={"(480) 962-7922"}
+    />
+    <MapView.Marker
+                    coordinate={{latitude: 33.5653,
+                    longitude:-112.10766}}
+                    title={"8825 N 23rd Ave., Ste. 100 Phoenix, AZ 85021"}
+                    description={"(480) 962-7922"}
+   />
