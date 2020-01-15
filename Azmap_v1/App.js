e8140c66378838a42820a6c8efb32b80f93bae37
raisa-khatun <raisakhatun@gmail.com>
2020-01-15 14:26:33 -0700

    Adding custom description when clicked on marker

--- Azmap_v1/App.js
+++ Azmap_v1/App.js
@@ -24,7 +24,7 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
+import {Callout, Marker} from 'react-native-maps';
 import MapView from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
@@ -34,32 +34,40 @@ const App: () => React$Node = () => {
 
   const [med_centers, setMed]=useState(require('./data/med_centers.json'));
   const [initialPosition, setInitialPosition]=useState({
-                                                        latitude: 33.45179,longitude: -112.022179,
-                                                        latitudeDelta: 0.9922,
-                                                        longitudeDelta: 0.9922});
+                                                        latitude: 0,longitude: 0,
+                                                        latitudeDelta: 0.0009,
+                                                        longitudeDelta: 0.0009,
+                                                        zoomEnabled:true});
+  const [currentLat, setCurrentLat]=useState(33.45179);
+  const [currentLong, setCurrentLong]=useState(-112.022179);
+  const [selectedPoint, setSelectedPoint]=useState(null);
 
-
-  const LATITUDE_DELTA = 0.0922
-  const LONGITUDE_DELTA = 0.9922
-
+  const window = Dimensions.get('window');
+  const { width, height }  = window
+  const LATITUDE_DELTA = 0.0030;
+  const LONGITUDE_DELTA=LATITUDE_DELTA + (width / height);
 
   useEffect(() => {
       Geolocation.getCurrentPosition((position) => {
             var lat = parseFloat(position.coords.latitude)
             var long = parseFloat(position.coords.longitude)
-
+            console.log("lat:",lat);
+            console.log("long:",long);
             var initialRegion = {
               latitude: lat,
               longitude: long,
               latitudeDelta: LATITUDE_DELTA,
               longitudeDelta: LONGITUDE_DELTA,
-
+              zoomEnabled:true
             }
             setInitialPosition(initialRegion);
+            setCurrentLat(lat);
+            setCurrentLong(long);
 
           },
           (error) => alert(JSON.stringify(error)),
           {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000});
+
 
 
 
@@ -73,17 +81,33 @@ const App: () => React$Node = () => {
         initialRegion={initialPosition}
 
     >
+   <Marker
+       coordinate={{latitude: currentLat, longitude:currentLong}}
+       title={"My location"}
+    />
    {med_centers.map(point => {
-    console.log(point.latitude);
-                       return  <MapView.Marker
+    //console.log(point.id);
+                       return  <Marker
                            key={point.id}
                            coordinate={{latitude: parseFloat(point.latitude), longitude:parseFloat(point.longitude)}}
-                           title={point.address}
-                           snippet={point.opiod}
-                           description={point.phone}
+                           onCalloutPress={() => {console.log("callout press")}}>
+                           <Callout>
+                                       <View>
+                                           <Text>{point.address}</Text>
+                                           <Text>{point.phone}</Text>
+                                           <Text>{point.opiod}</Text>
+                                           <Text>{point.website}</Text>
+                                       </View>
+                           </Callout>
 
-                         />
+                        </Marker>
+
+
     })}
+    {console.log("------------------------")}
+
+
+
 
    </MapView>
 
