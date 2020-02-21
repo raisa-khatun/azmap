3412fef8794bab80acef23db6e4f4cb6de43be04
Imran <imran@Macbooks-MacBook-Pro.local>
2020-02-21 12:50:14 -0700

    Adding SOS, Map page and navigation

---
+++ Azmap_v1/components/SOS.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Dimensions,
+  Button,
+  TouchableHighlight
+} from 'react-native';
+import { createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
+export default class SOS extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <TouchableHighlight
+              style ={styles.button}>
+              <Button onPress={this._onPressButton}
+              title="SOS"
+              accessibilityLabel="Learn more about this button"
+            />
+      </TouchableHighlight>
+      <TouchableHighlight
+            style ={styles.button}>
+           <Button
+             title="Go to Map"
+             onPress={() => this.props.navigation.navigate('Map')}
+           />
+      </TouchableHighlight>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1, alignItems: 'center', justifyContent: 'center'
+    },
+    button:
+        {
+            height: 40,
+            width:100,
+            borderRadius:10,
+            //backgroundColor:'red',
+            marginLeft :50,
+            marginRight:50,
+            marginTop :20
+        }
+
+
+
+});
--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 npm install react-native-maps --save-exact
 npm install @react-native-community/geolocation --save
 npm install --save react-native-google-maps-directions
+npm install react-navigation --save
+npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view --save
+
+
+comment line 20 to 22 in file /azmap/Azmap_v1/node_modules/react-native-maps/lib/android/build.gradle (if running in macos).
 
 npx react-native run-android
 
